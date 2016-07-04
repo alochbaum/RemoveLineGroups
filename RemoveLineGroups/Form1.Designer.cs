c4c8c5e6954907495131bf7baae382fc3efadd50
Andrew Lochbaum <Andrew.Lochbaum@grassvalley.com>
2016-07-04 11:39:41 -0400

    Setup

---
+++ RemoveLineGroups/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace RemoveLineGroups
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.btGo = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.PhraseInFirstLine = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PhraseInLastLine = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAddRow = new System.Windows.Forms.Button();
+            this.btnDelRow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(12, 17);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(60, 27);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Select";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(78, 21);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(557, 20);
+            this.tbFile.TabIndex = 1;
+            // 
+            // btGo
+            // 
+            this.btGo.Location = new System.Drawing.Point(641, 17);
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(60, 27);
+            this.btGo.TabIndex = 0;
+            this.btGo.Text = "Go";
+            this.btGo.UseVisualStyleBackColor = true;
+            this.btGo.Click += new System.EventHandler(this.btGo_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PhraseInFirstLine,
+            this.PhraseInLastLine});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(689, 194);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 283);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(689, 169);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // PhraseInFirstLine
+            // 
+            this.PhraseInFirstLine.HeaderText = "Phrase in first line of group";
+            this.PhraseInFirstLine.Name = "PhraseInFirstLine";
+            this.PhraseInFirstLine.ReadOnly = true;
+            this.PhraseInFirstLine.Width = 320;
+            // 
+            // PhraseInLastLine
+            // 
+            this.PhraseInLastLine.HeaderText = "Phrase in last line of group";
+            this.PhraseInLastLine.Name = "PhraseInLastLine";
+            this.PhraseInLastLine.ReadOnly = true;
+            this.PhraseInLastLine.Width = 320;
+            // 
+            // btnAddRow
+            // 
+            this.btnAddRow.Location = new System.Drawing.Point(12, 250);
+            this.btnAddRow.Name = "btnAddRow";
+            this.btnAddRow.Size = new System.Drawing.Size(76, 27);
+            this.btnAddRow.TabIndex = 0;
+            this.btnAddRow.Text = "Add Row";
+            this.btnAddRow.UseVisualStyleBackColor = true;
+            this.btnAddRow.Click += new System.EventHandler(this.btnAddRow_Click);
+            // 
+            // btnDelRow
+            // 
+            this.btnDelRow.Location = new System.Drawing.Point(94, 250);
+            this.btnDelRow.Name = "btnDelRow";
+            this.btnDelRow.Size = new System.Drawing.Size(76, 27);
+            this.btnDelRow.TabIndex = 0;
+            this.btnDelRow.Text = "Delete Row";
+            this.btnDelRow.UseVisualStyleBackColor = true;
+            this.btnDelRow.Click += new System.EventHandler(this.btnDelRow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 464);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.btGo);
+            this.Controls.Add(this.btnDelRow);
+            this.Controls.Add(this.btnAddRow);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "Remove Group of Lines From File";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Button btGo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PhraseInFirstLine;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PhraseInLastLine;
+        private System.Windows.Forms.Button btnAddRow;
+        private System.Windows.Forms.Button btnDelRow;
+    }
+}
+
---
+++ RemoveLineGroups/ModifyRow.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace RemoveLineGroups
+{
+    partial class ModifyRow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFirstRow = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPhraseLastRow = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phrase in first row";
+            // 
+            // tbFirstRow
+            // 
+            this.tbFirstRow.Location = new System.Drawing.Point(12, 25);
+            this.tbFirstRow.Name = "tbFirstRow";
+            this.tbFirstRow.Size = new System.Drawing.Size(556, 20);
+            this.tbFirstRow.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Phrase in last row";
+            // 
+            // tbPhraseLastRow
+            // 
+            this.tbPhraseLastRow.Location = new System.Drawing.Point(12, 64);
+            this.tbPhraseLastRow.Name = "tbPhraseLastRow";
+            this.tbPhraseLastRow.Size = new System.Drawing.Size(556, 20);
+            this.tbPhraseLastRow.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(492, 98);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(411, 98);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // ModifyRow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 133);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.tbPhraseLastRow);
+            this.Controls.Add(this.tbFirstRow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ModifyRow";
+            this.Text = "ModifyRow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFirstRow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPhraseLastRow;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
